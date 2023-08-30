1efc3d4d5e6f5d733eb96bb46cd99d09c62fe386
freitzzz <1160907@isep.ipp.pt>
2023-08-30 08:20:41 +0100

    one more test

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,12 +27,14 @@ detekt {
     autoCorrect = true
 }
 
-publishing {
-    /*val localProperties = Properties().apply {
+afterEvaluate {
+
+    publishing {
+        /*val localProperties = Properties().apply {
         load(FileInputStream(rootProject.file("local.properties")))
     }*/
 
-    /*repositories {
+        /*repositories {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/kotlin-artisans/${project.name}")
@@ -43,30 +45,30 @@ publishing {
         }
     }*/
 
-    publications {
-        register("release", MavenPublication::class) {
-            artifactId = project.name
-            groupId = "com.github.kotlin-artisans"
-            version = "0.0.1"
-            artifact("build/libs/${project.name}.jar")
-            File(".").list().forEach(::println)
-            println("------ | $buildDir")
+        publications {
+            register("release", MavenPublication::class) {
+                artifactId = project.name
+                groupId = "com.github.kotlin-artisans"
+                version = "0.0.1"
+                artifact("build/libs/${project.name}.jar")
+                File(".").list().forEach(::println)
+                println("------ | $buildDir")
+                components.forEach { println(it.name) }
 
-
-
-            /* Copied from: https://medium.com/@stpatrck/publish-an-android-library-to-github-packages-8dfff3ececcb */
-            pom {
-                withXml {
-                    val dependencies = asNode().appendNode("dependencies")
-                    configurations.implementation.get().dependencies.forEach {
-                        if (it.group != null &&
-                            "unspecified" != it.name &&
-                            it.version != null
-                        ) {
-                            val dependencyNode = dependencies.appendNode("dependency")
-                            dependencyNode.appendNode("groupId", it.group)
-                            dependencyNode.appendNode("artifactId", it.name)
-                            dependencyNode.appendNode("version", it.version)
+                /* Copied from: https://medium.com/@stpatrck/publish-an-android-library-to-github-packages-8dfff3ececcb */
+                pom {
+                    withXml {
+                        val dependencies = asNode().appendNode("dependencies")
+                        configurations.implementation.get().dependencies.forEach {
+                            if (it.group != null &&
+                                "unspecified" != it.name &&
+                                it.version != null
+                            ) {
+                                val dependencyNode = dependencies.appendNode("dependency")
+                                dependencyNode.appendNode("groupId", it.group)
+                                dependencyNode.appendNode("artifactId", it.name)
+                                dependencyNode.appendNode("version", it.version)
+                            }
                         }
                     }
                 }
