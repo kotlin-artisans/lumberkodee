705eafde74edd2fb1080810fbd3237c5d4564dcb
freitzzz <1160907@isep.ipp.pt>
2023-11-04 17:19:05 +0000

    fix: commit after running npx changeset

--- config/scripts/release.sh
+++ config/scripts/release.sh
@@ -2,3 +2,7 @@
 
 # Starts the release process by updating the package changelog.
 npx changeset
+
+# Requires manual stage + commit since @changeset/cli installed globally is not able to
+# resolve internal modules: https://github.com/changesets/changesets/issues/945
+git add .changeset/ && git commit -m "docs(changeset): release changeset" && git push
